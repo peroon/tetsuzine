cc9766592adebca72b459da67cbbe4182075e98c
Keita Ishibashi <fromjpn@gmail.com>
2013-11-26 01:20:26 +0900

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131124180709) do
+ActiveRecord::Schema.define(version: 20131125161148) do
 
   create_table "books", force: true do |t|
     t.string   "title"
     t.integer  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "published_on"
   end
 
 end
